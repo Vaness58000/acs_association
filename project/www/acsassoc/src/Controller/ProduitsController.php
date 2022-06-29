594772724bb16e9592759092db219a7fb1b2e0c0
poukette58 <vanessa.geoffroid@infamail.com>
2022-06-29 14:46:36 +0200

    suite image

--- project/www/acsassoc/src/Controller/ProduitsController.php
+++ project/www/acsassoc/src/Controller/ProduitsController.php
@@ -9,6 +9,7 @@ use App\Repository\ProduitsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -134,23 +135,27 @@ class ProduitsController extends AbstractController
     }
 
     /**
-     * @Route("/supprime/image/{id}, name="produits_delete_image", methods={"DELETE"})
+     * @Route("/supprime/image/{id}, name="app_produits_delete_image", methods={"DELETE"})
      */
-    /*public function deleteImage(Images $image, Request $request){
+    public function deleteImage(Images $image, Request $request){
         $data = json_decode($request->getContent(), true);
-        
-        //On vérifie si le token est valide
-        if($this->isCsrfTokenValid('delete' .$image->getId(), $data['_token'])){
-            //On récupère le nom de l'image
+    
+        // On vérifie si le token est valide
+        if($this->isCsrfTokenValid('delete'.$image->getId(), $data['_token'])){
+            // On récupère le nom de l'image
             $nom = $image->getName();
-            //On supprime le fichier
-            unlink($this->getParameter('image_directory'). '/' .$nom);
-
-            //on supprime l'entrée de la base 
+            // On supprime le fichier
+            unlink($this->getParameter('images_directory').'/'.$nom);
+    
+            // On supprime l'entrée de la base
             $em = $this->getDoctrine()->getManager();
             $em->remove($image);
             $em->flush();
+    
+            // On répond en json
+            return new JsonResponse(['success' => 1]);
+        }else{
+            return new JsonResponse(['error' => 'Token Invalide'], 400);
         }
-
-}*/
+    }
 }
