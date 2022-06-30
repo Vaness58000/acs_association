c6cfe022b247809bd7595cf646e2bb9a7d55ec78
poukette58 <vanessa.geoffroid@infamail.com>
2022-06-30 16:56:48 +0200

    suite page utilisateur

--- project/www/acsassoc/src/Controller/UsersController.php
+++ project/www/acsassoc/src/Controller/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class UsersController extends AbstractController
 {
@@ -13,8 +14,30 @@ class UsersController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
+        return $this->render('users/index.html.twig');
+    }
+
+ /**
+     * @Route("/users/profil/modifier", name="users_profil_modifier")
+     */
+    public function editProfile(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&& $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('users');
+        }
+        return $this->render('users/editprofile.html.twig', [
+            'form' => $form->createView(),
         ]);
+
     }
 }
